ff286b284e291fe37d053f53c1c055efd536effa
JontePonte <john_nordstrand@hotmail.com>
2020-10-28 12:00:00 +0100

    completed regular 010

---
+++ Regular/010.py
@@ -0,0 +1,27 @@
+'''
+In this bite you learn to catch/raise exceptions.
+
+Write a simple division function meeting the following requirements:
+
+when denominator is 0 catch the corresponding exception and return 0.
+when numerator or denominator are not of the right type reraise the corresponding exception.
+if the result of the division (after surviving the exceptions) is negative, raise a ValueError
+As always see the tests written in pytest to see what your code need to pass. Have fun!
+'''
+
+def positive_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+    
+    except ZeroDivisionError:
+        return 0
+
+    except TypeError:
+        raise TypeError
+
+    if result < 0:
+        raise ValueError
+
+    return result
+
+# print(positive_divide(1,0.5))
